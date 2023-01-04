9fc60bc7678ae5f9a314f5a148d1aeaf66079ec8
Yan Kaiky <yankaikys@gmail.com>
2023-01-04 07:33:17 -0300

    feat(CartPage): add floatingActionButton

--- lib/src/pages/cart/cart.page.dart
+++ lib/src/pages/cart/cart.page.dart
@@ -1,11 +1,14 @@
 import 'package:cafe/src/pages/cart/components/body.cart.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -31,6 +34,29 @@ class CartPage extends StatelessWidget {
         ],
       ),
       body: BodyCart(),
+      floatingActionButton: ElevatedButton(
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12.0),
+            ),
+          ),
+        ),
+        onPressed: () {},
+        child: SizedBox(
+          width: size.width / 5,
+          height: 50,
+          child: Center(
+            child: Text(
+              'Buy now',
+              style: GoogleFonts.poppins(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
